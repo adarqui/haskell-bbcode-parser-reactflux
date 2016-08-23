c2ce2436d1f4a6e224b35c167311cf78df79b9d8
Andrew Darqui <andrew.darqui@gmail.com>
2016-08-23 15:13:52 -0400

    slowly implementing the old purescript html stuff to haskell

--- src/Data/BBCode/HTML/ReactFlux.hs
+++ src/Data/BBCode/HTML/ReactFlux.hs
@@ -1,13 +1,18 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Data.BBCode.HTML.ReactFlux (
-  runBBCodeToHTML
+    runBBCodeToHTML
+  , runBBCodeToHTMLWith
+  , bbcodeToHTML
+  , codeToHTML
 ) where
 
 
 
-import React.Flux
-import Control.Monad.Trans.RWS
+import           Control.Monad.Trans.RWS
+import           React.Flux
 
-import Data.BBCode
+import           Data.BBCode
 
 
 
@@ -29,4 +34,43 @@ runBBCodeToHTMLWith parse_reader codes =
 bbcodeToHTML :: [BBCode] -> ParseEff HTMLView_
 bbcodeToHTML codes = go [] codes
   where
-  go acc xs = pure mempty
+  go acc []     = pure mempty
+  go acc (x:xs) = do
+    html <- codeToHTML x
+    go (html : acc) xs
+
+
+
+codeToHTML :: BBCode -> ParseEff HTMLView_
+codeToHTML tag = do
+  case tag of
+    Bold xs       -> strong_ <$> bbcodeToHTML xs
+    Italic xs     -> em_ <$> bbcodeToHTML xs
+    Underline xs  -> span_ [] <$> bbcodeToHTML xs -- style underline
+    Strike xs     -> del_ <$> bbcodeToHTML xs
+    Font opts xs  -> pure mempty
+    Size opts xs  -> pure mempty
+    Color opts xs -> pure mempty
+    Center xs     -> p_ [] <$> bbcodeToHTML xs -- align center
+    AlignLeft xs  -> pure mempty
+    AlignRight xs -> pure mempty
+    Quote author xs -> pure mempty
+    Link (Just name) url -> pure mempty
+    Link Nothing url -> pure mempty
+    List list     -> pure mempty
+    OrdList list  -> pure mempty
+    Table table   -> pure mempty
+    Pre text      -> pure $ pre_ $ elemText text
+    Code _ code   -> pure $ pre_ $ elemText code
+    Move xs       -> pure mempty
+    Text text     -> pure $ elemText text
+    Image opts url -> pure mempty
+    Youtube url    -> pure mempty
+    Vimeo url      -> pure mempty
+    Facebook url   -> pure mempty
+    Instagram url  -> pure mempty
+    Streamable url -> pure mempty
+    Imgur url      -> pure mempty
+    HR             -> pure $ hr_ mempty
+    NL             -> pure $ br_ mempty
+    _              -> pure $ p_ $ elemText "unknown"
