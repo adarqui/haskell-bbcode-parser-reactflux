fd40206e6b25517a8a26c8487f5fc703505bd100
Andrew Darqui <andrew.darqui@gmail.com>
2016-08-23 14:55:51 -0400

    getting started

--- src/Data/BBCode/HTML/ReactFlux.hs
+++ src/Data/BBCode/HTML/ReactFlux.hs
@@ -1,2 +1,32 @@
 module Data.BBCode.HTML.ReactFlux (
+  runBBCodeToHTML
 ) where
+
+
+
+import React.Flux
+import Control.Monad.Trans.RWS
+
+import Data.BBCode
+
+
+
+type HTMLView_ = ReactElementM ViewEventHandler ()
+
+
+
+runBBCodeToHTML :: [BBCode] -> HTMLView_
+runBBCodeToHTML = runBBCodeToHTMLWith defaultParseReader
+
+
+
+runBBCodeToHTMLWith :: ParseReader -> [BBCode] -> HTMLView_
+runBBCodeToHTMLWith parse_reader codes =
+  fst $ evalRWS (bbcodeToHTML codes) parse_reader defaultParseState
+
+
+
+bbcodeToHTML :: [BBCode] -> ParseEff HTMLView_
+bbcodeToHTML codes = go [] codes
+  where
+  go acc xs = pure mempty
